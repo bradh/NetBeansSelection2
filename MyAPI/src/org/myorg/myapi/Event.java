7e016ee3705470d4e7532e1265ae4dab8ca5118b
Brad Hards <bradh@frogmouth.net>
2020-12-22 12:08:55 +1100

    initial checkin

---
+++ MyAPI/src/org/myorg/myapi/Event.java
@@ -0,0 +1,27 @@
+package org.myorg.myapi;
+
+import java.time.ZonedDateTime;
+
+public final class Event {
+
+    private final ZonedDateTime date = ZonedDateTime.now();
+    private static int count = 0;
+    private final int index;
+
+    public Event() {
+        index = count++;
+    }
+
+    public ZonedDateTime getDate() {
+        return date;
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    @Override
+    public String toString() {
+        return index + " - " + date.toString();
+    }
+}
